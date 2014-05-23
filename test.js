6eb4bf8c871af6458e54e52f41b6d72dff69504c
Dallon Feldner <dallonfdev@gmail.com>
2014-05-22 21:05:40 -0500

    Made JavaScript scanner asynchronous

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 var scanjs = require('./lib/scanner/scanjs'),
     path = require('path');
 
-var files = scanjs(require('./js-dirs'), path.join(__dirname, 'public'));
-
-console.log(files);
+scanjs(require('./js-dirs'), path.join(__dirname, 'public'), function(err, files) {
+  if (err) {
+    console.error(err);
+    return;  
+  }
+  
+  console.log(files);
+});
