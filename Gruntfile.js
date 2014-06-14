b212863cb0649a414ba59125af6520f621190502
Dallon Feldner <dallonfdev@gmail.com>
2014-06-14 13:38:53 -0500

    Added LESS support and other grunt watching magic

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,34 @@ module.exports = function(grunt) {
       options: {
         livereload: true
       },
+      Gruntfile: {
+        files: 'Gruntfile.js',
+        options: { livereload: false }
+      },
       js: {
-        files: ['./public/js/**/*.js']
+        files: './public/js/**/*.js'
+      },
+      html: {
+        files: ['./index.ejs', './public/templates/**/*.html']
+      },
+      less: {
+        files: './public/less/**/*.less',
+        tasks: ['less'],
+        options: { livereload: false }
+      },
+      css: {
+        files: './public/css/**/*.css'
+      }
+    },
+    less: {
+      development: {
+        files: {
+          './public/css/stylesheet.css': './public/less/**/*.less'
+        }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
 };
